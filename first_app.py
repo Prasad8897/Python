09c7b5aedb8955b710bc5bfb5127b83d74b48bd8
Prasad8897 <codekarenge@gmail.com>
2017-11-24 22:15:36 +0530

    button added and message displayed"

--- first_app.py
+++ first_app.py
@@ -1,5 +1,14 @@
 import tkinter
+import tkMessageBox
 
+def message_to_the_world():
+	tkinter.Button(top,text="hello world",command=message_to_the_world, pady="50px").pack()
+	tkinter.Tk().mainloop()
+	
 top = tkinter.Tk()
+top.geometry("500x500")
+
+hello_world_button = tkinter.Button(top,text="hello world",command=message_to_the_world, pady="50px")
+hello_world_button.pack()
 
 top.mainloop()
