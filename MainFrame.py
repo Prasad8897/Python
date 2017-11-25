41072d2f4b45340674746aa26500fa66c2efa9f3
Prasad8897 <codekarenge@gmail.com>
2017-11-26 00:09:47 +0530

    learn a few more things like elements cant be called from class

---
+++ MainFrame.py
@@ -0,0 +1,9 @@
+import tkinter 
+class MainFrame():
+	def __init__(self, *args, **kwargs):
+		self.element = tkinter.Tk()
+		print "i just go fucked"
+
+	def object(self):
+		print "not again plz"
+		return self.element
--- first_app.py
+++ first_app.py
@@ -6,13 +6,4 @@ def message_to_the_world():
 	
 top = Tk()
 top.geometry("500x500")
-
-t = Checkbutton(top,text="pokemon",justify='left')
-
-t.place(x=0,y=0)
-t.place_forget()
-
-button = Button(top,text="pokemons",command=lambda:button.pack_forget())
-button.pack()
-
 top.mainloop()
