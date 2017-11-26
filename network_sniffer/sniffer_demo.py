9e4548749d389b5bedac049d37e9428811d72347
Prasad8897 <codekarenge@gmail.com>
2017-11-26 14:41:50 +0530

    learing network sniffing'

--- network_sniffer/sniffer_demo.py
+++ network_sniffer/sniffer_demo.py
@@ -3,3 +3,7 @@ import struct
 import textwarp
 
 #unpack ethernat frame
+def ethenet_frame(data):
+	dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+	return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto) , data[14:]
+
