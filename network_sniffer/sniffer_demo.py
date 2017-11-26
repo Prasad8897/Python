86eba745703908fcbb5c759350f99b8bc5ab547f
Prasad8897 <codekarenge@gmail.com>
2017-11-26 14:46:52 +0530

    network sniffing- formating mac address

--- network_sniffer/sniffer_demo.py
+++ network_sniffer/sniffer_demo.py
@@ -7,3 +7,8 @@ def ethenet_frame(data):
 	dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
 	return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto) , data[14:]
 
+#Return properly formatted mac address (ie AA:BB:CC:DD:EE:FF)
+def get_mac_addr(bytes_addr):
+	bytes_str = map('{:02x}'.format, bytes_addr)
+	mac_addr = ':'.join(bytes_str).upper()
+	return mac_addr
