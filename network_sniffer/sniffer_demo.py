9a1bc706c6cafac32625c00bd3667acb47516a98
Prasad8897 <codekarenge@gmail.com>
2017-11-27 11:47:10 +0530

    network_sniffer-unpacking of IP headers

--- network_sniffer/sniffer_demo.py
+++ network_sniffer/sniffer_demo.py
@@ -22,6 +22,17 @@ def get_mac_addr(bytes_addr):
 	mac_addr = ':'.join(bytes_str).upper()
 	return mac_addr
 
-def t(num):
-	return ("%02x" % num)
+# Unpacks IPv4 packtes
+def ipv4_packet(data):
+	version_header_length = data[0]
+	version = version_header_length >> 4
+	header_length = (version_header_length & 15) * 4
+	ttl, proto, src, target = struct.unpack("! 8x B B 2x 4s 4s", data[:20])
+	return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# Returns properly formatted IPv4 address
+def ipv4(addr):
+	return '.'.join(map('',addr))
+
+
 main()
