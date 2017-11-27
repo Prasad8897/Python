8c9b0e895cf2c577c73842b885adbac07ab09e9f
Prasad8897 <codekarenge@gmail.com>
2017-11-27 12:34:47 +0530

    network_sniffing-displaying packet data

--- network_sniffer/sniffer_demo.py
+++ network_sniffer/sniffer_demo.py
@@ -2,6 +2,16 @@ import socket
 import struct
 import textwrap
 
+TAB_1 = '\t   '
+TAB_2 = '\t\t   '
+TAB_3 = '\t\t\t   '
+TAB_4 = '\t\t\t\t   '
+
+DATA_TAB_1 = '\t '
+DATA_TAB_2 = '\t\t '
+DATA_TAB_3 = '\t\t\t '
+DATA_TAB_4 = '\t\t\t\t '
+
 def main():
 	conn = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.ntohs(3))
 
@@ -9,7 +19,21 @@ def main():
 		raw_data, addr = conn.recvfrom(65536)
 		dest_mac, src_mac, eth_proto, data = ethenet_frame(raw_data)
 		print ('\nEternat frame')
-		print ('Destination: {},Source: {},Protocol: {},'.format(dest_mac, src_mac, eth_proto))
+		print (TAB_1+'Destination: {},Source: {},Protocol: {},'.format(dest_mac, src_mac, eth_proto))
+
+		# 8 for IPv4
+		if eth_proto == 8:
+			version, header_length, ttl, proto, src, target, data = ipv4_packet(data)
+			print (TAB_1 + 'IPv4 Packets')
+			print (TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'format(version, header_length, ttl))
+			print (TAB_2 + 'Protocaol: {}, Source: {}, Target: {}'.format(proto,src,target))
+
+			if proto == 1:
+				icmp_type, code, checksum, data[4:] = icmp_packets(data)
+				print (TAB_1 + 'ICMP Packet')
+				print (TAB_2 + 'Type: {}, Code: {}, Checksum: {},'format(icmp_type, code, checksum))
+				print (TAB_2 + 'Data:')
+				print (format_multi_line(DATA_TAB_3,data))
 
 #unpack ethernat frame
 def ethenet_frame(data):
